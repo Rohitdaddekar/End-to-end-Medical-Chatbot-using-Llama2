70b3dea932431657e406ac3f6c1a22ed2a4072d0
Rohit <rohitdaddekar24@gmail.com>
2024-01-24 19:17:45 +0530

    All Folders Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Rohit Daddekar',
+    author_email= 'rohitdaddekar24@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
